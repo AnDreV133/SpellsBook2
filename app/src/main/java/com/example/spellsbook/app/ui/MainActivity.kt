4e9edf0098f71be97e2524402fb608b8d73f8e3f
AnDreV133 <dmitrijkubik@gmail.com>
2024-12-16 14:12:27 +0300

    refactor: clear unused sharedpref

--- app/src/main/java/com/example/spellsbook/app/ui/MainActivity.kt
+++ app/src/main/java/com/example/spellsbook/app/ui/MainActivity.kt
@@ -3,15 +3,6 @@ package com.example.spellsbook.app.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.example.spellsbook.app.ui.compose.MainWindow
 import com.example.spellsbook.app.ui.compose.navigation.AppNavHost
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,28 +10,8 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-//    @Inject
-//    lateinit var initDao: InitDao
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-//        val locale = when (Locale.current.language) {
-//            "ru" -> "ru"
-//            else -> "en"
-//        }
-
-//        getSharedPreferences("global", MODE_PRIVATE).run {
-//            getString(LAST_LOCALE_KEY, "")?.also { lastLocale ->
-//                lifecycleScope.launch(Dispatchers.IO) {
-//                    if (lastLocale != locale || initDao.hasSpells().not()) {
-//                        initDbByLocale(this@MainActivity, locale, initDao)
-//                    }
-//                }
-//            }
-//
-//            edit().putString(LAST_LOCALE_KEY, locale).apply()
-//        }
 
         setContent {
             MainWindow {
@@ -49,30 +20,4 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-@Composable
-fun ComposeHolder(
-    menuBar: @Composable () -> Unit,
-    content: @Composable () -> Unit
-) {
-    Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        bottomBar = {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(60.dp)
-            )
-            menuBar()
-        }
-    ) {
-        Box(
-            modifier = Modifier
-                .padding(it)
-        ) {
-            content()
-        }
-    }
-}
-
 
--- app/src/main/java/com/example/spellsbook/app/ui/MainApplication.kt
+++ app/src/main/java/com/example/spellsbook/app/ui/MainApplication.kt
@@ -5,15 +5,6 @@ import dagger.hilt.android.HiltAndroidApp
 
 open class CoreApplication : Application()
 
-val LAST_LOCALE_KEY = "last_locale"
-
 @HiltAndroidApp
-class MainApplication : CoreApplication() {
-    override fun onCreate() {
-        super.onCreate()
-
-        getSharedPreferences(LAST_LOCALE_KEY, MODE_PRIVATE)
-            .edit().putString(LAST_LOCALE_KEY, "").apply()
-    }
-}
+class MainApplication : CoreApplication()
 
